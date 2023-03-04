25d59033b859d2580a2573cadba15b7090c34717
maracuja0 <110384417+maracuja0@users.noreply.github.com>
2023-03-04 14:32:15 +0400

    Добавил сцену первого уровня, карту, спрайт игрока и физику взаимодейтсвия

---
+++ Elendil/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    private float vertical;
+    private float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rb.MovePosition(position);
+    }
+}
