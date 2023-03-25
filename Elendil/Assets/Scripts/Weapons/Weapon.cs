743e7a3892aee55f15877f67946a84070bd7dfa7
maracuja0 <110384417+maracuja0@users.noreply.github.com>
2023-03-26 00:55:37 +0400

    Добавил свет в лабиринт

--- Elendil/Assets/Scripts/Weapons/Weapon.cs
+++ Elendil/Assets/Scripts/Weapons/Weapon.cs
@@ -9,21 +9,7 @@ public class Weapon : MonoBehaviour
     public GameObject lightningPrefab;
     public float range = 10f;
     public int damage = 10;
-
-
-    // public float lightningRadius = 10f;
-    // public float lightningRange = 2f;
-    // public int maxBounces = 3;
-    // private float nextFireTime = 5f;
-
-    // private float nextFireTime = 0f;
     public GameObject nearestEnemy;
-    public GameObject NearestEnemy{
-        get
-        {
-            return this.nearestEnemy;
-        }
-    }
     
     void Update()
     {
@@ -35,13 +21,6 @@ public class Weapon : MonoBehaviour
             // Направляем оружие на ближайшего врага
             Vector2 direction = nearestEnemy.transform.position - transform.position;
             transform.up = direction;
-
-            // Стреляем, если нажата кнопка выстрела и прошло достаточно времени с предыдущего выстрела
-            // if (fireButtonCanvas.enabled && Time.time >= nextFireTime)
-            // {
-            //     Shoot();
-            //     nextFireTime = Time.time + fireRate;
-            // }
         }
     }
 
@@ -73,22 +52,10 @@ public class Weapon : MonoBehaviour
     {
         LightningBullet lightningScript = lightningPrefab.GetComponent<LightningBullet>();
         lightningScript.SetTarget(nearestEnemy);
+
         if(nearestEnemy != null){
             GameObject lightning = Instantiate(lightningPrefab, firePoint.position, firePoint.rotation);
         }
-        
     }
-
-
-
-    // public void LightningSkill(){
-    //     GameObject lightning = Instantiate(lightningPrefab, firePoint.position, firePoint.rotation);
-    //     int currentBounces = 0; 
-    //     while(currentBounces < maxBounces){
-    //         if (){
-
-    //         }
-    //     }
-    // }
 }
 
